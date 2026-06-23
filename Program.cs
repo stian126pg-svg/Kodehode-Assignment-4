db76514bdf8171ee88f1837be8ea73eb2f29a048
stian126jg-svg <stian1.26jg@gmail.com>
2026-06-23 13:37:08 +0200

    Program.cs added. Some things that still need to be added, but I have it ready.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using DigimonDatabase.Controllers;
+using DigimonDatabase.Services;
+
+var reader = new CsvReader();
+
+var digimons = reader.ReadDigimon(
+    @"Data\DigiDB_digimonlist.csv");
+
+var controller = new DigimonController(digimons);
+
+var rookies = controller.GetRookies();
+
+foreach (var rookie in rookies)
+{
+    Console.WriteLine(rookie.Name);
+}
