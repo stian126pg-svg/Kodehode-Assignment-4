382d9b8a50d38e7093596c2153acd5a30879af7e
stian126jg-svg <stian1.26jg@gmail.com>
2026-06-23 13:38:35 +0200

    Added Controllers.csv as well.

---
+++ Controllers/DigimonController.cs
@@ -0,0 +1,34 @@
+using DigimonDatabase.Models;
+
+namespace DigimonDatabase.Controllers
+{
+    public class DigimonController
+    {
+        private readonly List<Digimon> _digimons;
+
+        public DigimonController(List<Digimon> digimons)
+        {
+            _digimons = digimons;
+        }
+
+        public IEnumerable<Digimon> GetRookies()
+        {
+            return _digimons.Where(d => d.Stage == "Rookie");
+        }
+
+        public IEnumerable<string> GetNames()
+        {
+            return _digimons.Select(d => d.Name);
+        }
+
+        public IEnumerable<Digimon> GetByAttribute(string attribute)
+        {
+            return _digimons.Where(d => d.Attribute == attribute);
+        }
+
+        public IEnumerable<Digimon> GetMemoryAbove(int value)
+        {
+            return _digimons.Where(d => d.Memory > value);
+        }
+    }
+}
